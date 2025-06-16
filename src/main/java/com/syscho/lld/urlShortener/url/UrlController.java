9cd1f9f14bf8eac6766ed178420bf1ae25d45de8
Praveen Soni <praveen_soni@epam.com>
2025-06-16 17:16:32 +0530

    checkstyle added

--- src/main/java/com/syscho/lld/urlShortener/url/UrlController.java
+++ src/main/java/com/syscho/lld/urlShortener/url/UrlController.java
@@ -7,9 +7,11 @@ import com.syscho.lld.urlShortener.url.service.UrlService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/url")
@@ -25,6 +27,5 @@ public class UrlController {
     public ResponseEntity<UrlResponse> shortenUrl(@RequestBody UrlRequest request) {
         return ResponseEntity.ok(urlService.shortenUrl(request));
     }
-
 
 }
